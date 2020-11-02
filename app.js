de7ecde9f500fc89895ec680e0d7c59d649b3565
I066841 <a.kulshreshtha@sap.com>
2020-11-03 00:40:35 +0530

    How to call web-api via request API

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting the app')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('1st Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=39b7484969b5faa43a71c1af2e0e551d&%20query=Gwalior'
 
-setTimeout(()=> {
-    console.log('2nd Timer')
-}, 0)
-
-console.log('Stopping the app')
+request({url: url}, (error, response)=> {
+   const data = JSON.parse(response.body)
+   console.log(data.current)
+})
