7c61a9bcc88c10c21d85304501082d9dfd580514
I066841 <a.kulshreshtha@sap.com>
2020-11-03 23:12:32 +0530

    WeatherStack/Mapbox basic usage

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=39b7484969b5faa43a71c1af2e0e551d&%20query=Gwalior'
+// const url = 'http://api.weatherstack.com/current?access_key=39b7484969b5faa43a71c1af2e0e551d&%20query=Gwalior'
 
-request({url: url}, (error, response)=> {
-   const data = JSON.parse(response.body)
-   console.log(data.current)
+// request({url: url, json: true}, (error, response)=> {
+//    // const data = JSON.parse(response.body)
+//    // console.log(data.current)
+//    console.log(`Current temperature in ${response.body.location.name} is ${response.body.current.temperature} deg celsius. There is ${response.body.current.precip}% chance of rain. 
+//                 It feels like ${response.body.current.feelslike}`)
+// })
+
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Gwalior.json?access_token=pk.eyJ1IjoiYWJieWRkeWJidWsiLCJhIjoiY2toMjg2bDZyMDYyYjJ4bGlvOGU2ODVwMSJ9.gbSZP9gFl9a3hEX1vAxQBQ'
+request({url: geocodeUrl, json: true}, (error, response) => {
+    console.log(`Latitude of ${response.body.features[0].place_name} is ${response.body.features[0].center[1]}`)
+    console.log(`Longitude of ${response.body.features[0].place_name} is ${response.body.features[0].center[0]}`)
+   
 })
